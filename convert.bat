34568bd870a5f2c17f95b2fe17445017866dda61
Luna <luna@chloedev.de>
2025-02-25 14:27:13 +0100

    Fix bat-errors when file-directory contains whitespaces

--- convert.bat
+++ convert.bat
@@ -3,11 +3,11 @@ set WD=%cd%
 cd "%~dp0"
 set args=
 :loop
-if "%1"=="" goto execute
-set args=%args% "%1"
+if "%~1"=="" goto execute
+set args=%args% "%~1"
 shift
 goto loop
 :execute
 ".\.venv\Scripts\python" "__convert.py"%args%
-cd %WD%
+cd "%WD%"
 pause
